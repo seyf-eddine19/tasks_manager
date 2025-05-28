7a1254b52953454349a900c2be1d8352277402f7
Seyf Eddine <84080839+seyf-eddine19@users.noreply.github.com>
2025-05-28 20:43:27 +0100

    Update forms.py

--- projects/forms.py
+++ projects/forms.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User, Group, Permission
 from .models import Project, Task, UserProfile
 
 
+class UploadFileForm(forms.Form):
+    file = forms.FileField(label="تحميل ملف JSON")
+
 class UserForm(forms.ModelForm):
     password = forms.CharField(
         widget=forms.PasswordInput,
